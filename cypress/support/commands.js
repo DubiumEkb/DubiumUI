4e27d80cac00c27e067ef4bbc77ab3d04fb8209e
DubiumEkb <dubium-ekb@yandex.ru>
2025-02-01 21:07:53 +0500

    Clear gh-pages branch

--- cypress/support/commands.js
+++
@@ -1,7 +0,0 @@
-import "cypress-axe"
-
-// Добавляем команду для проверки доступности
-Cypress.Commands.add("checkAccessibility", () => {
-	cy.injectAxe() // Внедряем axe-core в страницу
-	cy.checkA11y() // Проверяем доступность
-})
