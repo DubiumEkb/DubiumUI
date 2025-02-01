66afac96d800837c87cc01ed970639892b756cad
DubiumEkb <dubium-ekb@yandex.ru>
2025-02-01 21:25:14 +0500

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { resolve } from "path"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
+	base: "/DubiumUI", // Указываем базовый путь для GitHub Pages
 	plugins: [react()],
 	build: {
 		lib: {
